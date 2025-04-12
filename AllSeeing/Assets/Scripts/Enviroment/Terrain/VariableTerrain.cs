8155be9cf0679aaa65af21174621fba2492c127f
Zanegerous <155862535+Zanegerous@users.noreply.github.com>
2025-04-12 00:46:46 -0500

    Made the global Variable that affects objects switching the colors
    Example scene showcases it. Took me longer than expected but it works and affects every instance of some objects. The code is there to program it in to scene objects.

---
+++ AllSeeing/Assets/Scripts/Enviroment/Terrain/VariableTerrain.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableTerrain : MonoBehaviour
+{
+
+    public VisualState requiredToBeActive;
+    private SpriteRenderer SpriteTexture;
+    private Collider2D objectCollider;
+
+
+
+    // Activated or deactivates the listener to the currentVisualManager that tracks the worlds color state
+    private void OnEnable()
+    {
+        CurrentVisualManager.Instance.OnVisualStateChanged += SwitchTerrainBasedOnColor;
+    }
+
+    private void OnDisable()
+    {
+        CurrentVisualManager.Instance.OnVisualStateChanged -= SwitchTerrainBasedOnColor;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteTexture = GetComponent<SpriteRenderer>();
+        objectCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void CheckRequired(VisualState state, Color stateColor)
+    {
+        bool activated = (requiredToBeActive == state);
+
+
+
+
+        if (activated && (SpriteTexture != null))
+        {
+            SpriteTexture.color = stateColor;
+        }
+        else
+        {
+            // have to do it this way because gameObject.SetActive(false) unsubscribes it.
+            // We may want to change the onDisable and handle it differently, but for now
+            // Im just doing this for testing purposes
+            SpriteTexture.color = Color.clear;
+            objectCollider.enabled = activated;
+            objectCollider.isTrigger = activated;
+
+        }
+    }
+
+
+    private void SwitchTerrainBasedOnColor(VisualState state)
+    {
+        switch (state)
+        {
+            case VisualState.RED_VIEW:
+                CheckRequired(state, Color.red);
+                break;
+
+            case VisualState.GREEN_VIEW:
+                CheckRequired(state, Color.green);
+                break;
+
+            case VisualState.BLUE_VIEW:
+                CheckRequired(state, Color.blue);
+                break;
+
+            case VisualState.COLORLESS_VIEW:
+                // Never should actually have a color, this doesnt affect 
+                // main scene
+                CheckRequired(state, Color.magenta);
+                break;
+        }
+
+    }
+}
---
+++ AllSeeing/Assets/Scripts/States/VisualState.cs
@@ -0,0 +1,8 @@
+public enum VisualState
+{
+    COLORLESS_VIEW,
+    RED_VIEW,
+    GREEN_VIEW,
+    BLUE_VIEW,
+
+}
