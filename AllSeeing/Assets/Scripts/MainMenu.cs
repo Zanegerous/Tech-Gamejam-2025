d64f12762fe6f1d159f27419230211033f50fd4f
Zoe <zoe052202@gmail.com>
2025-04-13 16:44:27 -0500

    Revert "sounds to every level"
    This reverts commit 471ae559913d49740bd72406103036d5a0bb5bcf.

--- AllSeeing/Assets/Scripts/MainMenu.cs
+++ AllSeeing/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
 
     public void PlayGame(){
-        SceneManager.LoadSceneAsync("LevelTwo");
+        SceneManager.LoadSceneAsync("LevelOne");
     }
    
    public void QuitGame(){
